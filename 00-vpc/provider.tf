7a680a3a191d84887c46819817b15783577800de
Supraja <supra556699@gmail.com>
2025-01-21 15:09:16 +0800

    expense infra using jenkins cicd

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.75.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "supra-remote-state-dev"
+    key            = "expense-vpc"
+    region         = "us-east-1"
+    dynamodb_table = "supra-locking-dev"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
